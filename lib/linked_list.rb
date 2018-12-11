276ca8b2c7addd2a646ca4d8172cb659bb04e996
Chris Wegscheid <cwegscheid08@outlook.com>
2018-12-11 18:49:43 -0500

    Functionality working

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,14 +12,106 @@ class LinkedList
 		@current_node = @current_node.next_node
 	end
 
+	def head
+		@current_node = @head
+	end
+
+	def tail
+		until @current_node.pointer == nil
+			self.next
+		end
+	end
+
+	def at(x)
+		i = 0
+		head
+		until i == x
+			i += 1
+			self.next
+		end
+		@current_node
+	end
+
 	def append(append_data)
 		if @current_node.pointer == nil
 			append_data = Node.new(append_data, nil)
 			@current_node.pointer = append_data
+		elsif @current_node.pointer != nil
+			self.next
+			append(append_data)
 		end
 	end
 
-end
+	def prepend(prepend_data)
+		@current_node = @head
+		prepend_data = Node.new(prepend_data, @current_node.next_node)
+		@current_node.pointer = prepend_data
+	end
 
-# list = LinkedList.new
-# list.append("Chris")
+	def size
+		head
+		i = 0
+		until @current_node.pointer == nil
+			i += 1
+			self.next
+		end
+		i
+	end
+
+	def pop
+		tail
+		val = @current_node
+		head
+		until @current_node.pointer == val
+			self.next
+		end
+		@current_node.pointer = nil
+		val
+	end
+
+	def contains?(value)
+		head
+		until @current_node.pointer == nil
+			if @current_node.value == value  
+				return true
+			end
+			self.next
+		end
+		false
+	end
+
+	def find(data)
+		head
+		i = 0
+		until @current_node.pointer == nil
+			if @current_node.value == data
+				return i
+			end
+			i += 1
+			self.next
+		end
+		nil
+	end
+
+	def to_s
+		head
+		until @current_node.pointer == nil
+			print "#{@current_node.value} -> "
+			self.next
+		end
+		print "#{@current_node.value}"
+	end
+
+	def insert_at(data, index)
+		at(index)
+		data = Node.new(data, @current_node.pointer)
+		@current_node.pointer = data
+	end
+
+	def remove_at(index)
+		at(index)
+		val = @current_node
+		at(index-1)
+		@current_node.pointer = val.next_node
+	end
+end
